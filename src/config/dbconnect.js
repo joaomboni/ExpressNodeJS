b9314fc16db8d76fb692191fce8fa244fa8e6216
joaomboni <joaoboni017@gmail.com>
2022-05-28 11:55:14 -0300

    Correção do Mongo

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://jbonifacio:12Hoquei34@cluster0.6tgcdqn.mongodb.net/projeto-node?");
+mongoose.connect("mongodb+srv://<usr>:<password>@cluster0.6tgcdqn.mongodb.net/<DB>?");
 
 let db = mongoose.connection;
 
